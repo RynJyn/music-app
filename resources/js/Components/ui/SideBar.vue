82a48f68b015c9d811951cf928c20b64cd43fa54
Ryan Jeynes <ryanjeynes@outlook.com>
2024-05-07 16:48:49 +0100

    Create Side Bar component

---
+++ resources/js/Components/ui/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <aside>
+        <slot></slot>
+    </aside>
+</template>
+
+<script>
+export default {
+    props: {
+        anchor: {
+            type: String,
+            default: "left"
+        }
+    }
+}
+</script>
