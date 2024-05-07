869f843de0a39d1f270a1317e7a5efb97c1a057f
Ryan Jeynes <ryanjeynes@outlook.com>
2024-05-07 16:49:53 +0100

    Create basic pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import StreamLayout from '../Layouts/StreamLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Home" />
+    <h1>Home</h1>
+</template>
+
+<script>
+export default {
+    props: {
+        canLogin: Boolean,
+        canRegister: Boolean
+    },
+    layout: StreamLayout
+}</script>
---
+++ resources/js/Pages/Search.vue
@@ -0,0 +1,15 @@
+<script setup>
+import StreamLayout from '../Layouts/StreamLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Search" />
+    <h1>Search</h1>
+</template>
+
+<script>
+export default {
+    layout: StreamLayout
+}
+</script>
