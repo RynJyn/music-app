0fd873087921f4d5e798445a0cb8e8d2182b37f7
Ryan Jeynes <ryanjeynes@outlook.com>
2024-05-08 13:00:17 +0100

    Define seeded user account in env

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => env('LOCAL_SEED_NAME'),
+            'email' => env('LOCAL_SEED_EMAIL'),
         ]);
     }
 }
