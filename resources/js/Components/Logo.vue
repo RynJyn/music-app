24004ca54955c86720a0c9e6190d0a1a3416dfaf
Ryan Jeynes <ryanjeynes@outlook.com>
2024-06-07 17:49:26 +0100

    Create Logo component

---
+++ resources/js/Components/Logo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="app-logo">
+        <!--Icons by FontAwesome-->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>
+        <span>music app</span>
+    </div>
+</template>
+
+<style>
+.app-logo {
+    display: flex;
+    gap: 1em;
+    align-items: center;
+}
+
+.app-logo svg {
+    height: 75px;
+    width: 75px;
+    fill: #ffdf7f;
+}
+
+.app-logo span {
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    font-size: 1.3rem;
+    color: #ffdf7f;
+    cursor: default;
+    user-select: none;
+}
+</style>
