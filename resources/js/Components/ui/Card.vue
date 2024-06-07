8f12300171bb340445769f9a58ff588b618f6bea
Ryan Jeynes <ryanjeynes@outlook.com>
2024-06-07 17:44:49 +0100

    Create Card component

---
+++ resources/js/Components/ui/Card.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <component class="card" :is="link ? Link : 'div'" :href="link">
+        <img v-if="!!img" width="200" height="200" :src="img.src" :style="{height: img.height + 'px', width: img.width + 'px'}"/>
+        <span>{{ title }}</span>
+    </component>
+</template>
+
+<script>
+export default {
+    props: {
+        link: String,
+        img: {
+            src: String,
+            height: {
+                type: String|Number,
+                default: 200
+            },
+            width: {
+                type: String|Number, 
+                default: 200
+            }
+        },
+        title: String
+    }
+}
+</script>
+
+<style>
+</style>
