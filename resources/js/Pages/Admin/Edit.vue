cd992f5246818e8ab918904798e3aea923830aab
Ryan Jeynes <ryanjeynes@outlook.com>
2024-06-07 17:56:46 +0100

    Create basic admin page

---
+++ resources/js/Pages/Admin/Edit.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AdminLayout from '../../Layouts/AdminLayout.vue';
+import AddArtistForm from './Partials/AddArtistForm.vue';
+import AddAlbumForm from './Partials/AddAlbumForm.vue';
+import AddTrackForm from './Partials/AddTrackForm.vue';
+</script>
+
+<template>
+    <Head title="Admin"/>
+    <h1>Admin</h1>
+
+    <AddArtistForm/>
+
+    <AddAlbumForm/>
+
+    <AddTrackForm/>
+</template>
+
+<script>
+export default {
+    layout: AdminLayout
+}
+</script>
