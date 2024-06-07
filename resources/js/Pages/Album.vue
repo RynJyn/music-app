5eea8ff80705a0e37e9709d9225a910afc1d59b8
Ryan Jeynes <ryanjeynes@outlook.com>
2024-06-07 17:57:13 +0100

    Create page to show a single album (public)

---
+++ resources/js/Pages/Album.vue
@@ -0,0 +1,25 @@
+<script setup>
+import StreamLayout from '../Layouts/StreamLayout.vue';
+import AlbumInfo from '@/Components/Stream/AlbumInfo.vue';
+import TrackListing from '@/Components/Stream/TrackListing.vue';
+
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head :title="album.title"/>
+    <AlbumInfo :artist="artist" :album="album" :trackCount="Object.keys(tracks).length">
+        <TrackListing :info="$props"/>
+    </AlbumInfo>
+</template>
+
+<script>
+export default {
+    layout: StreamLayout,
+    props: {
+        artist: Object,
+        album: Object,
+        tracks: Object
+    }
+}
+</script>
