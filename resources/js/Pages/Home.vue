746086ef923a598b7038c55102391c8de732f20f
Ryan Jeynes <ryanjeynes@outlook.com>
2024-06-07 17:47:33 +0100

    Add content to home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,18 +1,30 @@
 <script setup>
 import StreamLayout from '../Layouts/StreamLayout.vue';
+import CardSection from '@/Components/Stream/CardSection.vue';
+import Card from '@/Components/ui/Card.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Home" />
-    <h1>Home</h1>
+    <div class="card-sections">
+        <CardSection title="Latest Albums">
+            <Card v-for="album in albums" 
+            :key="album.id"
+            :link="route('album', {id: album.id})"
+            :img="{src: album.art}"
+            :title="album.title">
+            </Card>
+        </CardSection>
+    </div>
 </template>
 
 <script>
 export default {
     props: {
         canLogin: Boolean,
-        canRegister: Boolean
+        canRegister: Boolean,
+        albums: Object
     },
     layout: StreamLayout
 }</script>
