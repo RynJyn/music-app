e0fe1ebd1d01c55a5f0621eb9f84ae175b54284f
Ryan Jeynes <ryanjeynes@outlook.com>
2024-06-07 17:52:53 +0100

    Add route to fetch data about a single track

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/api/artists/', [ArtistController::class, 'query'])->name('artists.api');
 Route::get('/api/albums/', [AlbumController::class, 'query'])->name('albums.api');
+Route::get('/api/track/', [TrackController::class, 'single'])->name('track.api');
