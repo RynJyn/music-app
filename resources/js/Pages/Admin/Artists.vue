b6e2e37e0ea2a34e227ffc164ef929d7e96d1830
Ryan Jeynes <ryanjeynes@outlook.com>
2024-06-07 17:54:31 +0100

    Create page to show all artists

---
+++ resources/js/Pages/Admin/Artists.vue
@@ -0,0 +1,57 @@
+<script setup>
+import Paginated from '@/Components/ui/Paginated.vue';
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import Container from '@/Components/Container.vue';
+import { Link, Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head :title="`Artists | Page ${artists.current_page}`"/>
+    <div id="artists">
+        <div class="artist-details" v-for="artist of artists.data" :key="artist.id">
+            <Container>
+                <p>{{ artist.id }}</p>
+                <p>{{ artist.name }}</p>
+                <Link :href="route('artist.edit', artist)">Edit</Link>
+            </Container>
+        </div>
+
+        <Container>
+            <Paginated :links="artists.links"/>
+        </Container>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: AdminLayout,
+    props: {
+        artists: Object
+    }
+}
+</script>
+
+<style>
+div#artists {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+}
+
+div.artist-details div {
+    display: flex;
+    justify-content: space-between;
+    padding: 1em;
+    background: #333;
+    font-weight: 600;
+    color: #eee;
+    border: 1px solid #ffdf7f;
+    border-radius: 0.3em;
+}
+
+div.artist-details div a {
+    font-weight: 600;
+    text-decoration: underline;
+    color: #ffdf7f;
+}
+</style>
